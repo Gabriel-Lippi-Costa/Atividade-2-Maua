16463a27f1215b0fa717407e50ec16e4211ca7f4
Gabriel Lippi <gabriellippidacosta@gmail.com>
2025-02-22 11:18:19 -0300

    Site Completo

---
+++ animacao-scroll.js
@@ -0,0 +1,26 @@
+const observerH1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const observerApresentacao = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show-dois');
+        } else {
+            entry.target.classList.remove('show-dois');
+        }
+    });
+});
+
+
+const h1 = document.querySelectorAll('h1');
+const apresentacao = document.querySelectorAll('.img-sobre-nos');
+
+h1.forEach((element) => observerH1.observe(element));
+apresentacao.forEach((element) => observerApresentacao.observe(element));
